58e683b3403778b3ffcb23c9a4dac4d35b035f1f
mendesviton <vitor6890@gmail.com>
2022-11-21 15:21:50 -0300

    change: changing date validator

--- src/presentation/middleware/customer/customer_schema.ts
+++ src/presentation/middleware/customer/customer_schema.ts
@@ -1,14 +1,27 @@
 import * as yup from 'yup'
 import ICustomer from '../../../domain/entity/customer_entity'
-import { parse, isDate } from 'date-fns'
+import { parse } from 'date-fns'
 
-function parseDateString(value: any, originalValue: any) {
-   const parsedDate = isDate(originalValue) === true
-      ? originalValue
-      : parse(originalValue, "dd/MM/yyyy", new Date())
+// const isoDate = ({ format = 'yyyy-MM-dd' } = {}) =>
+//    Date().ransform(function (_, originalValue) {
+//       // Parses the date using `date-fns` `parse` method if it's not nil
+//       return maybeParseDate(format, originalValue)
+//    })
 
-   return parsedDate
-}
+
+// function _parseDateString(originalValue: string) {
+
+//    if (isDate(originalValue) === true) {
+//       return originalValue   
+//    } else {
+//       return (parse(originalValue, "dd/MM/yyyy", new Date()))
+//    }
+//    // const parsedDate = isDate(originalValue) === true
+//    //    ? originalValue
+//    //    : parse(originalValue, "dd/MM/yyyy", new Date())
+
+//    // return parsedDate
+// }
 
 enum cpfMaxMin { 'max' = 14, 'min' = 11 };
 enum phoneMaxMin { 'max' = 15, 'min' = 11 };
@@ -23,7 +36,15 @@ export  const bodyValidation: yup.SchemaOf<ICustomer> = yup.object().shape({
    email: yup.string().required().email(),
    email_confirmation: yup.string().required().email(),
    address: yup.string().required(),
-   birthdate: yup.date().transform(parseDateString).max(today).required(),
+   birthdate: yup.date()
+      .transform(function (value, originalValue) {
+         if (this.isType(value)) {
+            return value
+         }
+         const result = parse(originalValue, "dd.MM.yyyy", new Date())
+         return result
+      })
+      .required().max(today),
    cellphone: yup.string().min(phoneMaxMin.min).max(phoneMaxMin.max).required(),
    city: yup.string().required(),
    whatsapp: yup.bool().required(),
