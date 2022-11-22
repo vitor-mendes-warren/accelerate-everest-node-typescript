08ffc0ba55b815fbc60d3ca274ec33ed0787a983
mendesviton <vitor6890@gmail.com>
2022-11-22 16:19:53 -0300

    style: remove spaces not alowed

--- src/presentation/controllers/customer_controller.ts
+++ src/presentation/controllers/customer_controller.ts
@@ -2,14 +2,12 @@ import { Request, Response } from 'express'
 import ICustomer from '../../domain/entity/customer_entity'
 import CustomerService from '../../domain/services/customer_service'
 
-const  customerList: ICustomer[] = [] 
+const customerList: ICustomer[] = []
 
 export default class CustomerController{
-
-  static async handle(req: Request, res: Response) {
+  async handle (req: Request, res: Response): Promise<Response> {
     const userService = new CustomerService(customerList)
     const response = await userService.create(req.body)
     return res.status(response.code).json(response.data)
-
-   }
+  }
 }
