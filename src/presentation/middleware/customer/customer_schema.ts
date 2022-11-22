3424886567ae889e2a1a669505373186ae06348c
mendesviton <vitor6890@gmail.com>
2022-11-22 16:32:26 -0300

    style: remove spaces not alowed

--- src/presentation/middleware/customer/customer_schema.ts
+++ src/presentation/middleware/customer/customer_schema.ts
@@ -6,30 +6,26 @@ enum cpfMaxMin { 'max' = 14, 'min' = 11 };
 enum phoneMaxMin { 'max' = 15, 'min' = 11 };
 enum postalCodeMaxMin { 'max' = 9, 'min' = 8 };
 
+const today = new Date()
 
-
-const today = new Date() 
-
-export  const bodyValidation: yup.SchemaOf<ICustomer> = yup.object().shape({
-   full_name: yup.string().required().min(5),
-   email: yup.string().required().email(),
-   email_confirmation: yup.string().required().email(),
-   address: yup.string().required(),
-   birthdate: yup.date()
-      .transform(function (value, originalValue) {
-         if (this.isType(value)) {
-            return value
-         }
-         const result = parse(originalValue, "dd.MM.yyyy", new Date())
-         return result
-      })
-      .required().max(today),
-   cellphone: yup.string().min(phoneMaxMin.min).max(phoneMaxMin.max).required(),
-   city: yup.string().required(),
-   whatsapp: yup.bool().required(),
-   email_sms: yup.bool().required(),
-   country: yup.string().required(),
-   cpf: yup.string().min(cpfMaxMin.min).max(cpfMaxMin.max).required(),
-   postal_code: yup.string().min(postalCodeMaxMin.min).max(postalCodeMaxMin.max).required(),
-
+export const bodyValidation: yup.SchemaOf<ICustomer> = yup.object().shape({
+  full_name: yup.string().required().min(5),
+  email: yup.string().required().email(),
+  email_confirmation: yup.string().required().email(),
+  address: yup.string().required(),
+  birthdate: yup.date().transform(function (value, originalValue) {
+    if (this.isType(value)) {
+      return value
+    }
+    const result = parse(originalValue, 'dd.MM.yyyy', new Date())
+    return result
+  })
+    .required().max(today),
+  cellphone: yup.string().min(phoneMaxMin.min).max(phoneMaxMin.max).required(),
+  city: yup.string().required(),
+  whatsapp: yup.bool().required(),
+  email_sms: yup.bool().required(),
+  country: yup.string().required(),
+  cpf: yup.string().min(cpfMaxMin.min).max(cpfMaxMin.max).required(),
+  postal_code: yup.string().min(postalCodeMaxMin.min).max(postalCodeMaxMin.max).required()
 })
