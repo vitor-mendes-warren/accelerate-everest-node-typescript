9764419f8ee5af485238cd323392c4259dfa2916
mendesviton <vitor6890@gmail.com>
2022-12-14 16:25:19 -0300

    test: refact UserController test

--- src/presentation/controllers/create_customer_controller.test.ts
+++ src/presentation/controllers/create_customer_controller.test.ts
@@ -6,15 +6,18 @@ import { mockRequest, mockResponse } from 'src/utils/mock';
 jest.mock('src/domain/customer/services/create_customer_service')
 jest.mock('src/domain/customer/repository/customer_repository')
 jest.mock('src/domain/customer/model/customer')
-const customerServiceMock = CustomerService as jest.Mock<CustomerService>
-const req = mockRequest 
-const res = mockResponse
-describe('UserController', () => {
 
-  it('should StatusCode be 200 when handle() is calls', () => {
-    const customerService = new customerServiceMock()
-    const createCustomerController = new CreateCustomerController(customerService)
-    createCustomerController.handle(req, res)
-    expect(res.statusCode).toBe(200)
+const customerServiceMock = CustomerService as jest.Mock<CustomerService>
+const req = mockRequest
+const res = mockResponse
+
+describe('UserController', () => {
+  describe('handle', () => {
+    it('should StatusCode be 200 when handle() is calls with valid body', () => {
+      const customerService = new customerServiceMock()
+      const createCustomerController = new CreateCustomerController(customerService)
+      createCustomerController.handle(req, res)
+      expect(res.statusCode).toBe(200)
+    })
   })
 })
