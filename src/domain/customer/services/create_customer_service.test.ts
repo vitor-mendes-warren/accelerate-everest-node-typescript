56e92f04d525d9c8236f406a177f554a464dd873
mendesviton <vitor6890@gmail.com>
2022-12-14 16:21:54 -0300

    test: refact CustomerService test

--- src/domain/customer/services/create_customer_service.test.ts
+++ src/domain/customer/services/create_customer_service.test.ts
@@ -4,15 +4,23 @@ import CustomerRepository from "../repository/customer_repository"
 import CustomerService from "./create_customer_service"
 
 jest.mock('src/domain/customer/model/customer')
+
+
 const customerMock = Customer as jest.Mock<Customer>
 const customerRepositoryMock = CustomerRepository as jest.Mock<CustomerRepository>
+
 describe('CustomerService', () => {
-   it('should call CustomerRepository.create() when CustomerService.create() is called ', () => {
-   
-      const customer = new customerMock()
-      const customerRepository = new customerRepositoryMock()
-      const customerService = new CustomerService(customerRepository)
-      customerService.create(customer)
-      expect(customerRepository.create).toBeCalled
+   describe('Create', () => {
+
+      it('should call create with valid body when CustomerRepository.create is called ', () => {
+
+         const customer = new customerMock()
+         const customerRepository = new customerRepositoryMock()
+         const customerService = new CustomerService(customerRepository)
+         customerService.create(customer)
+         expect(customerRepository.create).toBeCalled
+      })
+
+
    })
 })
