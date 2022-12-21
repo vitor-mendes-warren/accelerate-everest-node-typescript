a885fe791c0398a7c51f6794c88f75b84ede44fa
mendesviton <vitor6890@gmail.com>
2022-12-21 11:27:32 -0300

    feat: adding swagger config and route doc config

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import './shared/container/'
 import { config } from 'dotenv'
 import express, { Express, json } from 'express'
 import router from './presentation/routes/routes'
+
 export const app: Express = express()
 
 app.use(json())
 app.use(router)
-
 config({ path: './config/config.env' })
 const port = process.env.PORT ?? 8000
 
--- src/presentation/routes/customer_router.ts
+++ src/presentation/routes/customer_router.ts
@@ -1,12 +1,11 @@
-
 import { Router } from 'express'
 import CreateCustomerController from '../../presentation/controllers/create_customer_controller'
 import customerMiddleware from '../../presentation/middleware/customer/customer_middleware'
 import { container } from 'tsyringe'
 
 const customerRouter = Router()
-const createUserController = container.resolve(CreateCustomerController)
 
+const createUserController = container.resolve(CreateCustomerController)
 customerRouter.post('/', customerMiddleware, createUserController.handle)
 
 export default customerRouter
