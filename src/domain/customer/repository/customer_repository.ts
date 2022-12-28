56342c7098f126b3a81f8f48ce4c35a068aab0fc
mendesviton <vitor6890@gmail.com>
2022-12-28 00:38:07 -0300

    feat: adding get update and delete paths

--- src/domain/customer/repository/customer_repository.ts
+++ src/domain/customer/repository/customer_repository.ts
@@ -1,31 +1,31 @@
 import IRepository from '@interfaces/domain/repository/repository'
+import { UpdateFilter } from 'mongodb'
 import { Schema } from 'mongoose'
-import MongoDBClient from 'src/infrastructure/mongodb/mongodb_client'
-import { inject, injectable } from 'tsyringe'
+import { injectable } from 'tsyringe'
 import Customer from '../model/customer'
 import customer_schema from '../model/schema/customer_schema'
 
 @injectable()
 export default class CustomerRepository implements IRepository<Customer> {
-  constructor (
-    @inject('DatabaseClient')
-    private readonly dbClient: MongoDBClient
-  ) { }
+  public async getAll (): Promise<Customer[]> {
+    return await customer_schema.find()
+  }
 
-  read: (id: Schema.Types.ObjectId) => Promise<Customer | null>
-  update: (id: Schema.Types.ObjectId) => Promise<Customer | null>
-  readAll: () => Promise<[Customer]>
-  delete: (id: Schema.Types.ObjectId) => Promise<Customer>
+  public async delete (_id: Schema.Types.ObjectId): Promise<Customer | null> {
+    return await customer_schema.findOneAndDelete(_id)
+  }
 
   public async create (customer: Customer): Promise<void> {
     await customer_schema.create(customer)
-    // const CustomerModel = this.dbClient.getInstance().model('customerDataSchema', customerDataSchema)
-    // const collection = new CustomerModel(customer)
-    // await collection.save()
   }
 
-  // public async read (id: Schema.Types.ObjectId): Promise<Customer | null> {
-  //   const customerModel = this.dbClient.getInstance().model('customerDataSchema', customerDataSchema)
-  //   return await customerModel.findOne(id)
-  // }
+  public async getById (_id: Schema.Types.ObjectId): Promise< Customer | null > {
+    return await customer_schema.findOne(_id)
+    
+  }
+
+  public async update(_id: Schema.Types.ObjectId, update: UpdateFilter<Document>): Promise<Customer | null> {
+    return await customer_schema.updateOne({ _id }, update);
+  }
+  
 }
--- src/interfaces/domain/repository/repository.ts
+++ src/interfaces/domain/repository/repository.ts
@@ -1,9 +1,4 @@
-import { ObjectId } from 'mongoose'
 
 export default interface IRepository<T> {
   create: (entity: T) => void
-  read: (id: ObjectId) => Promise<T | null>
-  update: (id: ObjectId) => Promise<T | null>
-  readAll: () => Promise<[T]>
-  delete: (id: ObjectId) => Promise<T>
 }
